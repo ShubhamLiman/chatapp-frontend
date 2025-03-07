b918707ec40d3a28a90624e8bbc0efa4fc01d1b3
Shubham Liman <shubhamlstudymail@gmail.com>
2025-03-07 10:41:24 +0530

    -

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  // baseURL: "https://chatapp-backend-production-196a.up.railway.app/api",
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://chatapp-backend-production-196a.up.railway.app/api",
+  // baseURL: "http://localhost:3000/api",
   // baseURL: "https://chatapp-backend-sigma.vercel.app/api",
   withCredentials: true,
 });
